e8984866945fa97281244b6758e98e0a2ed90e75
Srishti Gajare <srisgajare7@gmail.com>
2020-03-24 22:42:23 -0500

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,65 @@
+#import math
+
+#function to calculate gcd to check if multiplier and mod are co-primes or not 
+def gcd(m,n):
+    while n != 0:
+        temp = n
+        n = m % n
+        m = temp
+    return m
+
+#function to compute multiplicative inverse of multiplier
+def multiplicative_inverse(multiplier,mod):
+    x = 0
+    old_x = 1
+    y = 1
+    old_y = 0
+    while mod != 0:
+        temp = mod
+        quotient = multiplier // mod
+        mod = multiplier % mod
+        multiplier = temp
+        temp = x
+        x = old_x - quotient * x
+        old_x = temp
+        temp = y
+        y = old_y - quotient * y
+        old_y = temp
+    return old_x % 252526  #return positive inverse value
+
+#Read the cipher text from the file   
+with open("ciphertext.txt", "r") as f:
+    cipherText = f.read()
+    #print(cipherText)
+
+#Decrypt function to decrypt the ciphertext read from file 
+def decrypt(cipherText,inverse,b):
+    final_decrypted_output = ""
+    for i in range(0,len(cipherText), 7):  #picking 6 numbers including space to decrypt every block
+        decrypt = str((inverse * (int(cipherText[i:i+6]) - b)) % m).rjust(6,'0')  #formula to decrypt 
+        #print(decrypt)
+        for j in range(0,6,2): #each block has 6 digits where 2 digits represent number corresponding to a letter  
+            character = chr(int(decrypt[j:j+2]) + ord('A'))  #add 65 to determine the ascii value obtained and hence the character
+            final_decrypted_output += character 
+    print(f"The decrypted message is %s" %final_decrypted_output)
+
+    #Write the decrypted message to outputfile
+    with open("finalplaintextoutput.txt", "w") as f:
+        f.write(final_decrypted_output)
+
+
+if __name__ == '__main__':
+    m = 252526
+    a = int(input("Input multiplier for Block Affine cipher, this should be same as the one used for encryption: "))
+    b = int(input("Input offset for Block Affine cipher, this should be same as the one used for encryption: "))
+    if b > m:
+        print(f"ERROR:Offest entered not in range")
+        exit(0)
+    if gcd(a,m) != 1:
+        print(f"ERROR: The values of multiplier and modulus are not co-primes")
+        exit(0)
+    else:
+        mod_inverse = multiplicative_inverse(a,m)
+    #print(mod_inverse)
+    y = decrypt(cipherText,mod_inverse,b)
+    print("The output is written to finalplaintextoutput.txt file!")
---
+++ encrypt.py
@@ -0,0 +1,59 @@
+#import math
+import re
+
+#Read data from plaintext file and eliminate special characters including whitespaces,lowercase letters and numbers
+x = 'plaintext.txt'
+with open(x, "r+b") as file:
+     message = file.read()
+     #file.seek(0)
+     #file.write(message.upper())
+     #file.close()
+uncleanText = open(x).read()
+regex = re.compile('[\\\\@_,!.\'\"#$%^&*()<>?/|}{~:]')
+if(regex.search(uncleanText) != None): #if any of the above characters present in plaintext throw error
+    print("Input contains special characters, all the special characters,lowercase letters and white spaces will be eliminated") 
+plainText = re.sub('[^A-Z]+', '', uncleanText)  #substitute every character with space except A-Z
+open(x, 'w').write(plainText)
+print(f"The plaintext is %s" %plainText)  #prints the final clean plaintext to console
+
+#function to calculate gcd to check if multiplier and mod are co-primes or not 
+def gcd(m,n):
+    while n != 0:
+        temp = n
+        n = m % n
+        m = temp
+    return m
+
+#function to encrypt the plaintext
+def encrypt(pt,a,b):
+    m = 252526
+    if gcd(a,m) != 1:
+        print(f"ERROR: The values of multiplier and modulus are not co-primes")
+        exit(0)
+    final_cipher = ""
+    #Divide the input/plaintext into 3 character blocks
+    for i in range(0,len(pt),3):
+        output = ""       #save converted plaintext(alphabets) to equivalent numbers  
+        for j in range(0,3):  #characters picked in blocks of 3
+            p = (ord(pt[i+j])- ord('A'))  #Subtract from 65 to start assigning values from 0
+            p = str(p).rjust(2,'0')  #Append 0 for numbers from 1 to 9
+            output = output + str(p)
+        #print(output)
+        #formula to encrypt the plaintext
+        cipher = (a * int(output) + b) % m
+        #print(cipher)
+        final_cipher = final_cipher + str(cipher).rjust(6,'0') + " "  #appending all the blocks together and ensuring the values are 6 digits
+    #print(final_cipher)
+
+    #write the cipher text to ciphertext.txt
+    with open("ciphertext.txt", "w") as f:
+        f.write(final_cipher)
+
+if __name__ == '__main__':
+    a = int(input("Input multiplier for Block Affine cipher: "))
+    b = int(input("Input offset for Block Affine cipher: "))
+    if b > 252526:
+        print(f"ERROR: Offest entered not in range")
+        exit(0)
+    y = encrypt(plainText,a,b)
+    print("The ciphertext is written to ciphertext.txt file!")
